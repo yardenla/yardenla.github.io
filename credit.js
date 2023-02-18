119ef452382e7e75f9efeaf4687cfd7a54a3f961
Yardenla <yarden11598@gmail.com>
2023-02-18 11:02:11 +0200

    remove node.js qr

--- credit.js
+++ credit.js
@@ -1,19 +1,3 @@
-const scanner = new Html5QrcodeScanner("reader", {
-  qrbox: {
-    width: 250,
-    height: 250,
-  },
-  fps: 20,
-});
-
-scanner.render(success, error);
-
-function success(result) {
-  alert(result);
-}
-
-function error() {}
-
 // Set starting points and added points
 var points = 0;
 var newPoints;
