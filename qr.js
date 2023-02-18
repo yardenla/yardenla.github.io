e811d360e0a8429cc7af0edc6353e86e5533b65d
Yardenla <yarden11598@gmail.com>
2023-02-18 12:11:17 +0200

    qr

--- qr.js
+++ qr.js
@@ -1,18 +1,14 @@
-let qrCode = window.qrcode;
-console.log(qrCode);
-let scanIcon = document.querySelector("#scan");
-
 const video = document.createElement("video");
-const canvasElement = document.getElementById("canvas");
+const canvasElement = document.getElementById("qr-canvas");
 const canvas = canvasElement.getContext("2d");
 
-const qrResult = document.getElementById("result");
-const outputData = document.getElementById("output");
+const qrResult = document.getElementById("qr-result");
+const outputData = document.getElementById("outputData");
+const btnScanQR = document.getElementById("btn-scan-qr");
 
 let scanning = false;
 qrcode.callback = (res) => {
   if (res) {
-    //in here you can pass your data  "res" to an endpoint or something
     outputData.innerText = res;
     scanning = false;
 
@@ -21,41 +17,39 @@ qrcode.callback = (res) => {
     });
 
     qrResult.hidden = false;
-
+    btnScanQR.hidden = false;
     canvasElement.hidden = true;
-    alert(res);
   }
 };
-scanIcon.onclick = () => {
-  if (navigator.mediaDevices) {
-    navigator.mediaDevices
-      .getUserMedia({ video: { facingMode: "environment" } })
-      .then(function (stream) {
-        scanning = true;
-        qrResult.hidden = true;
 
-        canvasElement.hidden = false;
-        video.setAttribute("playsinline", true);
-        video.srcObject = stream;
-        video.play();
-        tick();
-        scan();
-      });
-  } else {
-    output.innerHTML = `Sorry, Your Browser Cannot support this feature`;
-  }
+btnScanQR.onclick = () => {
+  navigator.mediaDevices
+    .getUserMedia({ video: { facingMode: "environment" } })
+    .then(function (stream) {
+      scanning = true;
+      qrResult.hidden = true;
+      btnScanQR.hidden = true;
+      canvasElement.hidden = false;
+      video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+      video.srcObject = stream;
+      video.play();
+      tick();
+      scan();
+    });
 };
+
 function tick() {
-  canvasElement.height = video.videoHeight;
-  canvasElement.width = video.videoWidth;
-  canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-
-  scanning && requestAnimationFrame(tick);
-}
-function scan() {
-  try {
-    qrCode.decode();
-  } catch (e) {
-    setTimeout(scan, 300);
+    canvasElement.height = video.videoHeight;
+    canvasElement.width = video.videoWidth;
+    canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+  
+    scanning && requestAnimationFrame(tick);
   }
-}
+
+  function scan() {
+    try {
+      qrcode.decode();
+    } catch (e) {
+      setTimeout(scan, 300);
+    }
+  }
