97e218558709ca310990772aa69a2d8bdc99ff95
Yardenla <yarden11598@gmail.com>
2023-03-04 18:09:20 +0200

    changes

--- credit.js
+++ credit.js
@@ -1,28 +1,31 @@
 // Set starting points and added points
 var points = 0;
-var newPoints;
+var newPoints = 0;
 var cardNumbers = ["01", "02", "03"];
+var cardNumber = "";
+// set the points in the card
+cardNumbers.forEach((element) => localStorage.setItem(element, points));
 
 function GetCardNumber() {
   let cardNumber = prompt("הכניס.י מספר כרטיס");
   // if card number is in the list return card number
   if (cardNumbers.includes(cardNumber)) {
-    console.log(cardNumber);
+    //console.log(cardNumber);
     return cardNumber;
   } else {
     // alert card is not valid
     alert("כרטיס לא נמצא");
-    return;
   }
 }
 
 function InsertPoints() {
-  let cardNumber = GetCardNumber();
+  cardNumber = GetCardNumber();
   if (cardNumber) {
+    // set the points varabile with the key value
+    points = localStorage[cardNumber];
     // set the points in the card
     localStorage.setItem(cardNumber, points);
-    // set the points varabile with the key value
-    points = localStorage.getItem(cardNumber);
+    console.log(points, cardNumber);
     // get user input (points)
     newPoints = prompt("הכנס.י מספר נקודות");
     // add the new points to the old points
@@ -35,19 +38,20 @@ function InsertPoints() {
       "הטענת את הכרטיס ב-" + newPoints + " נקודות. יש לך " + points + " נקודות";
     // alert results
     alert(msg);
+
   }
 }
 
 function CheckPoints() {
   // get card number from user
-  let cardNumber = GetCardNumber();
+  cardNumber = GetCardNumber();
   // get results div
   let resultDiv = document.getElementById("checkPointsResult");
 
   // if card number is valid
   if (cardNumber) {
     // set the points varabile with the key value
-    let pointsLeft = localStorage.getItem(cardNumber);
+    let pointsLeft = localStorage[cardNumber];
     // if not been set set to 0
     if (pointsLeft == null) {
       pointsLeft = 0;
@@ -62,6 +66,8 @@ function CheckPoints() {
 function CleanLocalStorage() {
   // clear local storage data
   window.localStorage.clear();
+  points = 0;
+  newPoints = 0;
   // give success msg
   alert("נתונים נוקו בהצלחה");
 }
