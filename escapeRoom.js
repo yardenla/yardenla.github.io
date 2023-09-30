6508fb061ba6b4502ec857acac12dd9837c95e67
Yardenla <yarden11598@gmail.com>
2023-09-30 09:10:03 +0300

    new changes

--- escapeRoom.js
+++ escapeRoom.js
@@ -1,20 +1,38 @@
 const solveBtn = document.getElementById("solve-button");
 const puzzleForm = document.getElementById("puzzle-form");
 const loginErrorMsg = document.getElementById("puzzle-error-msg");
+const num1 = puzzleForm.num1;
+const num2 = puzzleForm.num2;
+const num3 = puzzleForm.num3;
+const num4 = puzzleForm.num4;
+
+function changeColor(element) {
+  element.style.color = "red";
+}
 
 solveBtn.addEventListener("click", (e) => {
   e.preventDefault();
-
-  const num1 = puzzleForm.num1.value;
-  const num2 = puzzleForm.num2.value;
-  const num3 = puzzleForm.num3.value;
-  const num4 = puzzleForm.num4.value;
-  console.log(num1 + num2 + num3 + num4);
-  if (num1 === "1" && num2 === "2" && num3 === "3" && num4 === "4") {
+  console.log(num1.value + num2.value + num3.value + num4.value);
+  if (
+    num1.value === "1" &&
+    num2.value === "2" &&
+    num3.value === "3" &&
+    num4.value === "4"
+  ) {
     alert("You have successfully logged in.");
-
-    //location.reload();
   } else {
     loginErrorMsg.style.opacity = 1;
+    num1.style.color = "red";
+    num2.style.color = "red";
+    num3.style.color = "red";
+    num4.style.color = "red";
   }
 });
+
+function changeBack() {
+  console.log("change");
+  num1.style.color = "black";
+  num2.style.color = "black";
+  num3.style.color = "black";
+  num4.style.color = "black";
+}
